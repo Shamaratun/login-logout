e738af896eb22fe523e458917ebe45cf13ba6a05
Shamaratun <shamaratunmeme@gmail.com>
2025-04-28 10:14:58 +0600

    add component

--- src/app/pages/customer-view/customer-view.component.ts
+++ src/app/pages/customer-view/customer-view.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-customer-view',
-  imports: [],
+  imports: [CommonModule, NgFor, NgIf],
   templateUrl: './customer-view.component.html',
   styleUrl: './customer-view.component.css'
 })
 export class CustomerViewComponent {
+  resources: { [key: string]: string } = {};
+  resourceKeys: string[] = [];
 
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit(): void {
+    this.loadResources();
+  }
+
+  loadResources() {
+    this.CustomerService.getResources().subscribe({
+      next: (data) => {
+        this.resources = data;
+        this.resourceKeys = Object.keys(data); // <== create keys here
+      },
+      error: (err) => {
+        console.error('Failed to load resources:', err);
+      }
+    });
+  }
 }

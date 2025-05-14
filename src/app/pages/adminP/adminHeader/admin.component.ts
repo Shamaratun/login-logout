5b4ab6c1a51c8988b7069618bd0760975a443e7a
Shamaratun <shamaratunmeme@gmail.com>
2025-05-15 00:58:04 +0600

    SEARCH NOT COMPLETE

--- src/app/pages/adminP/adminHeader/admin.component.ts
+++ src/app/pages/adminP/adminHeader/admin.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BookItemsComponent } from '../../book/book-items/book-items.component';
+import { Books } from '../../../models/book.model';
+import { BookService } from '../../../core/service/book.service';
+
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
 
+  books: Books[] = [];
+  selectedBook: Books | null = null;
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.bookService.getBooks().subscribe({
+      next: (data) => {
+        this.books = data;
+      },
+      error: (err) => {
+        console.error('Failed to load books:', err);
+      }
+    });
+  }
 }

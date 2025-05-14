7796bc523c1a06b0793f7380ef4b48fc728d11a0
Shamaratun <shamaratunmeme@gmail.com>
2025-05-15 00:58:18 +0600

    SEARCH NOT COMPLETE

--- src/app/pages/adminP/adminHeader/admin.component.ts
+++ src/app/pages/adminP/adminHeader/admin.component.ts
@@ -23,7 +23,7 @@ export class AdminComponent implements OnInit {
     this.loadBooks();
   }
 
-  loadBooks() {
+  loadBooks(): void {
     this.bookService.getBooks().subscribe({
       next: (data) => {
         this.books = data;
